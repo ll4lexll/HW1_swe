c8356c8ea6fc0d2a523365ddf93e26f4f1af1edd
sapirtalk <70822441+sapirtalk@users.noreply.github.com>
2021-05-31 20:17:41 +0300

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,6 +1,37 @@
+import java.util.ArrayList;
+
 public class Card {
     private int value;
-    private int kind;
+    private Shape s;
+
+
+    public int getValue(){
+        return this.value;
+    }
+    public Void setValue(int value){
+        this.value = value;
+    }
+    public Shape getShape() {
+        return this.s;
+    }
+    public Void setShape(Shape s){
+        this.s = s;
+    }
+
+    public Card(int value, Shape s){
+        this.setShape(s);
+        this.setValue(value);
+    }
+    public int compare (Card other){
+        if(this.getValue() < getValue(other)) return -1;
+        if(this.getValue() == getValue(other)) return 0;
+        if(this.getValue() > getValue(other)) return 1;
+    }
+    public String toString(){
+        int value = this.getValue();
+        
+
+    }
 
 
 }
