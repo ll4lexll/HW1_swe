372ddaeea1d6fc8b6ed20adb5cb52ae610b91f15
sapirtalk <70822441+sapirtalk@users.noreply.github.com>
2021-05-31 20:43:47 +0300

    Finished Card class

--- src/Card.java
+++ src/Card.java
@@ -4,7 +4,10 @@ public class Card {
     private int value;
     private Shape s;
 
-
+    public Card(int value, Shape s) {  //Card constructor
+        this.setShape(s);
+        this.setValue(value);
+    }
     public int getValue(){
         return this.value;
     }
@@ -18,18 +21,22 @@ public class Card {
         this.s = s;
     }
 
-    public Card(int value, Shape s){
-        this.setShape(s);
-        this.setValue(value);
+    public int compare (Card other){ //Compares this Card to another
+        if(this.getValue() < other.getValue()) return -1;
+        if(this.getValue() == other.getValue()) return 0;
+        if(this.getValue() > other.getValue()) return 1;
     }
-    public int compare (Card other){
-        if(this.getValue() < getValue(other)) return -1;
-        if(this.getValue() == getValue(other)) return 0;
-        if(this.getValue() > getValue(other)) return 1;
-    }
-    public String toString(){
-        int value = this.getValue();
-        
+    public String toString(){ //return the value and shape as a String
+        String type = this.getShape().toString();
+        switch (this.getValue()){
+            case 1:  return ("Ace of " + type);
+            case 11: return ("Jack of " + type);
+            case 12: return ("Queen of " + type);
+            case 13: return ("King of " + type);
+        }
+    return (this.getValue() + " of " + type);
+
+
 
     }
 
