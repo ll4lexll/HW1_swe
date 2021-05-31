03585d4742c043490f35c199ab35a52f9a89c782
sapirtalk <70822441+sapirtalk@users.noreply.github.com>
2021-05-31 19:06:29 +0300

    2

---
+++ src/Card.java
@@ -0,0 +1,6 @@
+public class Card {
+    private int value;
+    private int kind;
+
+
+}
---
+++ src/Shape.java
@@ -0,0 +1,24 @@
+public enum Shape {
+    /* enum for cards shape */
+    Spades(){
+        public String toString() {
+            return toString("♠");
+        }
+    }
+    Spades(){
+        public String toString() {
+            return toString("♠");
+        }
+    }
+    Spades(){
+        public String toString() {
+            return toString("♠");
+        }
+    }
+    Spades(){
+        public String toString() {
+            return toString("♠");
+        }
+    }
+
+}
