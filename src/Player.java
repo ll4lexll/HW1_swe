76a26fc5dc0bcee6c501eec770ca938624c9b00f
sapirtalk <70822441+sapirtalk@users.noreply.github.com>
2021-06-02 12:31:21 +0300

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -3,18 +3,18 @@ public class Player {
     private Deck draw_deck;
     private Deck win_deck;
 
-    public Player(String name) {
+    public Player(String name) {  //building a new player with empty decks
         this.draw_deck = new Deck(false);
         this.win_deck = new Deck(false);
     }
 
-    public String setName(String name) {
+    public String setName(String name) { //set player name
         this.name = name;
     }
 
     public String toString() {
         return this.name;
-    }
+    } //returns the name of the player
 
     public boolean outOfCards() { //check if the player has no cards
         if (this.draw_deck.isEmpty() && this.win_deck.isEmpty())
@@ -25,7 +25,12 @@ public class Player {
         this.draw_deck.removeTopCard();
     }
 
-    public void addCard
+    public void toDraw(Card card){ //adds a card to the draw deck
+      draw_deck.addCard(card);
+    }
+    public void toWin(Card card){  // adds a card to the win deck
+        win_deck.addCard(card);
+    }
 
 
 
